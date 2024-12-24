8c33b76fef3ddfa3faa54d6fc179b581827ce0b7
Thulani Tyaphile <tj.tyaphile@gmail.com>
2024-12-24 08:43:17 +0200

    Add new column

--- src/main/java/za/co/tyaphile/tenants/controller/ui/WebController.java
+++ src/main/java/za/co/tyaphile/tenants/controller/ui/WebController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import za.co.tyaphile.tenants.service.UserService;
 
 @Controller
@@ -17,4 +18,9 @@ public class WebController {
         model.addAttribute("users", _userService.getUsers());
         return "index";
     }
+
+//    @GetMapping("/building/{id}")
+//    public String building(Model model, @PathVariable("id") String id) {
+//
+//    }
 }
